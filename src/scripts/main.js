4095fb0cfb9ace02e9f79d79192a65a4922fd5ff
Sasha Kozlovskyy <alex33507@gmail.com>
2021-09-08 18:08:50 +0300

    Coffee

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
+
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.testimonials-slider', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 130,
+  pagination: false,
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+  loop: true,
+});
+
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-unused-vars
+const swip = new Swiper('.insta-slider', {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  freeMode: true,
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+
+    700: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+
+    1100: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+  },
+});
